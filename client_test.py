25f85442505213af836a06ef6e3c75bf9f3c7816
Russell Taylor <rdtaylorjr@protonmail.com>
2020-12-03 03:59:13 -0600

    Create Patch File

--- client_test.py
+++ client_test.py
@@ -22,15 +22,30 @@ class ClientTest(unittest.TestCase):
 
 
   """ ------------ Add more unit tests ------------ """
-  def test_getRatio(self):
-    prices = [
-      {'price_a': 121.2, 'price_b': 120.48},
-      {'price_a': 121.68, 'price_b': 117.87},
-      {'price_a': 119.2, 'price_b': 120.48},
-      {'price_a': 119.2, 'price_b': 121.68}
-    ]
-    for price in prices:
-      self.assertEqual(getRatio(price['price_a'], price['price_b']), price['price_a'] / price['price_b'])
+  def test_getRatio_priceBZero(self):
+    price_a = 119.2
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
+ 
+  def test_getRatio_priceAZero(self):
+    price_a = 0
+    price_b = 121.68
+    self.assertEqual(getRatio(price_a, price_b), 0)
+ 
+  def test_getRatio_greaterThan1(self):
+    price_a = 346.48
+    price_b = 166.39
+    self.assertGreater(getRatio(price_a, price_b), 1)
+ 
+  def test_getRatio_LessThan1(self):
+    price_a = 166.39
+    price_b = 356.48
+    self.assertLess(getRatio(price_a, price_b), 1)
+ 
+  def test_getRatio_exactlyOne(self):
+    price_a = 356.48
+    price_b = 356.48
+    self.assertEqual(getRatio(price_a, price_b), 1)
 
 
 if __name__ == '__main__':
